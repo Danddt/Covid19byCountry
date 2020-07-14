00750abd51777302d40cec0bf5a664cd404c23c0
Danddt <dndnewton@gmail.com>
2020-07-14 09:32:14 +0300

    changed date from 2018 to 2019 to reflect change in the json file

--- covid1.py
+++ covid1.py
@@ -1,4 +1,4 @@
-
+#packages
 import tkinter as tk
 import requests
 import json
@@ -22,29 +22,37 @@ def format_response(source):
                 deaths.append(item['deaths'])
                 if item['countriesAndTerritories'] not in country:
                     country.append(item['countriesAndTerritories'])
-                if item['popData2018'] not in pop:
-                    pop.append(item['popData2018'])
+                if item['popData2019'] not in pop:
+                    pop.append(item['popData2019'])
 
-        cases = [int(i) for i in cases] 
+        cases = [int(i) for i in cases]
+        negs =[]
+        for i in cases:
+            if i <0:
+                i = 0
+                negs.append(i)
+            else:
+                negs.append(i)
+        
         deaths = [int(i) for i in deaths]
         pop = int(pop [0])
         country = str(country[0])
 
-        tot_cases =sum((cases))
+        tot_cases =sum((negs))
         tot_deaths = sum((deaths))
         per_cent_c =round((tot_cases/(pop)*100),2)
         per_cent_d = round((tot_deaths/(tot_cases)*100),2)
 
 
 
-        final_str =  f'{country}\n\nRecorded cases: {tot_cases:,d} \n\nDeaths: {tot_deaths:,d}\n\nEstimated population in 2018: {pop:,d}\n\n% Cases in population: {per_cent_c}\n\n% Mortality in recorded cases: {per_cent_d}'
+        final_str =  f'{country}\n\nRecorded cases: {tot_cases:,d} \n\nDeaths: {tot_deaths:,d}\n\nEstimated population in 2019: {pop:,d}\n\n% Cases in population: {per_cent_c}\n\n% Mortality in recorded cases: {per_cent_d}'
     except:
         final_str = 'There was a problem retrieving that information'
 
     return final_str
 
 def get_data():
-    url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/'
+    url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json'
     response = requests.get(url)
     source = response.json()
     
@@ -53,17 +61,22 @@ def get_data():
 
 
 root = tk.Tk()
-root.title('Covid19 by country')
+root.title('Covid 19 data by country')
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg = '#a9dbe7')
 canvas.pack()
 
-background_image = tk.PhotoImage('background.png')
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+canvas.create_text(220,40,fill="#262262",font="Times 10 italic bold",
+                        text="Enter country/state code e.g UK for United Kingdom.")
+
+canvas.create_text(220,450,fill="black",font="Times 10 italic bold",
+                        text="Source: https://opendata.ecdc.europa.eu/covid19")
+
+
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+frame.place(relx=0.5, rely=0.1, relwidth=0.85, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font=40)
 entry.place(relwidth=0.65, relheight=1)
@@ -72,9 +85,11 @@ button = tk.Button(frame, text="Get Data", font=40, command=get_data)
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.85, relheight=0.6, anchor='n')
+
 
 label = tk.Label(lower_frame, font =  15 , anchor  = 'nw', justify ='left', bd = 5 )
 label.place(relwidth=1, relheight=1)
+w = tk.Label(root, text="Hello Tkinter!")
 
 root.mainloop()
